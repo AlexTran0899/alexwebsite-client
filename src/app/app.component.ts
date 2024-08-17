5755de9374f5849d2438cbe9c702cefdb6e62467
Alex Tran <alextran0899@gmail.com>
2024-08-17 17:24:23 -0500

    fix hero sizing issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeroInfoComponent } from './hero-info/hero-info.component';
 import { ImageCardComponent } from './image-card/image-card.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectDisplayComponent } from './project-display/project-display.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroInfoComponent, ImageCardComponent, NgbModule],
+  imports: [RouterOutlet, HeroInfoComponent, ImageCardComponent, NgbModule, ProjectDisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/project-display/project-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-display.component.html',
+  styleUrl: './project-display.component.css'
+})
+export class ProjectDisplayComponent {
+
+}
