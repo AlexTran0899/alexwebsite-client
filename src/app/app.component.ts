6beb15e62cd090a687d13c2524e7cbd108e2ddbb
Alex Tran <alextran0899@gmail.com>
2024-08-17 17:05:24 -0500

    add social buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroInfoComponent } from './hero-info/hero-info.component';
+import { ImageCardComponent } from './image-card/image-card.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroInfoComponent, ImageCardComponent, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/hero-info/hero-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hero-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-info.component.html',
+  styleUrl: './hero-info.component.css'
+})
+export class HeroInfoComponent {
+
+}
