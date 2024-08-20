12d889e5e568f828bf4d866b52a9844a41e8e2fd
Alex Tran <alextran0899@gmail.com>
2024-08-20 18:56:00 -0500

    add programmatic navigation to info buttons

--- src/app/hero-info/hero-info.component.ts
+++ src/app/hero-info/hero-info.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './hero-info.component.css'
 })
 export class HeroInfoComponent {
-
+  openLink(url: string): void {
+    window.open(url, '_blank');
+  }
 }
